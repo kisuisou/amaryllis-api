31acd49c9a3d8281764bc289f61480648d3487f1
kisui <63273180+kisuisou@users.noreply.github.com>
2025-06-14 17:01:54 +0000

    ふりがなを追加

--- model/book.go
+++ model/book.go
@@ -1,14 +1,16 @@
 package model
 
 type Book struct {
-	ISBN      string `gorm:"primaryKey"`
-	Title     string
-	Volume    string
-	Creator   string
-	NDC9      string
-	NDC10     string
-	NDLC      string
-	Publisher string
-	PubYear   int
-	Image     string
+	ISBN                 string `gorm:"primaryKey"`
+	Title                string
+	TitleTranscription   string
+	Volume               string
+	Creator              string
+	CreatorTranscription string
+	NDC9                 string
+	NDC10                string
+	NDLC                 string
+	Publisher            string
+	PubYear              int
+	Image                string
 }
